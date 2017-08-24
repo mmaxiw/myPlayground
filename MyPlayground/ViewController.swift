85c55bfb791a621411e327114f06e0c0b6505f3f
MaxiMiliano Wainberg <maximilianowainberg@maximilianosmbp>
2017-08-24 13:20:55 +0300

    Initial Commit

---
+++ MyPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPlayground
+//
+//  Created by MaxiMiliano Wainberg on 24/08/2017.
+//  Copyright © 2017 MaxiMiliano Wainberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
